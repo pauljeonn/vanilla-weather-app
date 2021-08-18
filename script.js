6ee660ce4fe8a71a21dff4029b16c3b3a8828948
Paul Jeon <pauljeonn@gmail.com>
2021-08-19 00:50:09 +0900

    assign weather data to html elements

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ let weather = {
 				this.apiKey
 		)
 			.then((response) => response.json())
-			.then((data) => console.log(data));
+			.then((data) => this.displayWeather(data));
+	},
+	displayWeather: function (data) {
+		const { name } = data;
+		const { icon, description } = data.weather[0];
+		const { temp, humidity } = data.main;
+		const { speed } = data.wind;
+		console.log(name, icon, description, temp, humidity, speed);
+		document.querySelector('.city').innerHTML = 'Weather in ' + name;
+		document.querySelector('.icon').src =
+			'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+		document.querySelector('.description').innerText = description;
+		document.querySelector('.temp').innerText = temp + '°C';
+		document.querySelector('.humidity').innerText =
+			'Humidity: ' + humidity + '%';
+		document.querySelector('.wind').innerText =
+			'Wind speed: ' + speed + ' km/h';
 	},
 };
